e6f99fb5f190f440b94eef0b75c9342a49725c1b
iAverages <31937542+iAverages@users.noreply.github.com>
2023-07-14 09:15:08 +0100

    Make downloads concurrent

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -53,52 +53,57 @@ func (b Bot) MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	urls := rx.FindAllString(m.Content, -1)
+	if len(urls) == 0 {
+		return
+	}
 
-	for _, url := range urls {
-		message, err := s.ChannelMessageSend(m.ChannelID, "Downloading...")
-		if err != nil {
-			b.logger.Errorw("Error sending message", "error", err)
-			return
-		}
+	for _, _url := range urls {
+		go func(url string) {
+			message, err := s.ChannelMessageSend(m.ChannelID, "Downloading...")
+			if err != nil {
+				b.logger.Errorw("Error sending message", "error", err)
+				return
+			}
 
-		cmd := exec.Command("yt-dlp", "-j", "--no-simulate", "-o", "%(upload_date)s-%(id)s.%(ext)s", url)
-		cmd.Dir = TEMP_DIR
-		out, err := cmd.Output()
-		if err != nil {
-			b.logger.Errorw("Error downloading video", "error", err)
-			s.ChannelMessageEdit(m.ChannelID, message.ID, "Error downloading video")
-			return
-		}
+			cmd := exec.Command("yt-dlp", "-j", "--no-simulate", "-o", "%(upload_date)s-%(id)s.%(ext)s", url)
+			cmd.Dir = TEMP_DIR
+			out, err := cmd.Output()
+			if err != nil {
+				b.logger.Errorw("Error downloading video", "error", err)
+				s.ChannelMessageEdit(m.ChannelID, message.ID, "Error downloading video")
+				return
+			}
 
-		ytdlVideo := YTDLVideo{}
-		json.Unmarshal(out, &ytdlVideo)
+			ytdlVideo := YTDLVideo{}
+			json.Unmarshal(out, &ytdlVideo)
 
-		b.logger.Infow("Downloaded video", "filename", ytdlVideo.Filename)
+			b.logger.Infow("Downloaded video", "filename", ytdlVideo.Filename)
 
-		text := "Uploading to CDN..."
-		s.ChannelMessageEdit(m.ChannelID, message.ID, text)
-		file, err := os.Open(TEMP_DIR + "/" + ytdlVideo.Filename)
-		if err != nil {
-			b.logger.Errorw("Error opening file", "error", err)
-			s.ChannelMessageEdit(m.ChannelID, message.ID, "Error uploading file")
-			return
-		}
-		defer file.Close()
+			text := "Uploading to CDN..."
+			s.ChannelMessageEdit(m.ChannelID, message.ID, text)
+			file, err := os.Open(TEMP_DIR + "/" + ytdlVideo.Filename)
+			if err != nil {
+				b.logger.Errorw("Error opening file", "error", err)
+				s.ChannelMessageEdit(m.ChannelID, message.ID, "Error uploading file")
+				return
+			}
+			defer file.Close()
 
-		name, err := uploader.UploadFile(file)
-		if err != nil {
-			b.logger.Errorw("Error opening file", "error", err)
-			s.ChannelMessageEdit(m.ChannelID, message.ID, "Error uploading file")
-			return
-		}
+			name, err := uploader.UploadFile(file)
+			if err != nil {
+				b.logger.Errorw("Error opening file", "error", err)
+				s.ChannelMessageEdit(m.ChannelID, message.ID, "Error uploading file")
+				return
+			}
 
-		text = "Done!, here is the file: " + b.config.HostUrl + name
-		messageEdit := &discordgo.MessageEdit{
-			Content: &text,
-			ID:      message.ID,
-			Channel: message.ChannelID,
-		}
+			text = "Done!, here is the file: " + b.config.HostUrl + name
+			messageEdit := &discordgo.MessageEdit{
+				Content: &text,
+				ID:      message.ID,
+				Channel: message.ChannelID,
+			}
 
-		s.ChannelMessageEditComplex(messageEdit)
+			s.ChannelMessageEditComplex(messageEdit)
+		}(_url)
 	}
 }
