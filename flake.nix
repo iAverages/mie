035d3fefd5308f2bc79d8a481d3bfa4658d46114
iAverages <me@danielraybone.com>
2025-08-22 02:38:38 +0900

    package app with nix and create docker image

--- flake.nix
+++ flake.nix
@@ -1,53 +1,78 @@
 {
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
-    rust-overlay.url = "https://flakehub.com/f/oxalica/rust-overlay/*.tar.gz";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
+    self,
     nixpkgs,
+    flake-utils,
     rust-overlay,
     ...
-  }: let
-    allSystems = [
-      "x86_64-linux"
-    ];
+  }:
+    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux"] (system: let
+      version = "0.1.0";
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
 
-    forEachSystem = f:
-      nixpkgs.lib.genAttrs allSystems (system:
-        f {
-          inherit system;
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlays.default
-            ];
+      buildDeps = with pkgs; [
+        pkg-config
+        perl
+      ];
+
+      devDeps = with pkgs; [
+        gcc
+        just
+        (rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+          ];
+        })
+      ];
+      runtimeDeps = with pkgs; [
+        openssl
+        ffmpeg
+        cacert
+      ];
+    in {
+      packages = {
+        default = pkgs.rustPlatform.buildRustPackage {
+          inherit version;
+          pname = "mie";
+          src = ./.;
+          # cargoLock = {
+          #   lockFile = ./Cargo.lock;
+          # };
+          cargoDeps = pkgs. rustPlatform.importCargoLock {
+            lockFile = ./Cargo.lock;
           };
-        });
-  in {
-    devShells = forEachSystem ({
-      pkgs,
-      system,
-    }: {
-      default = pkgs.mkShell {
-        shellHook = ''
-          export  PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
-        '';
-        packages = with pkgs; [
-          pkg-config
-          openssl
-          ffmpeg
-          gcc
-          just
-          (rust-bin.stable.latest.default.override {
-            extensions = [
-              "rust-src"
-              "rust-analyzer"
-            ];
-            targets = ["aarch64-unknown-linux-gnu"];
-          })
-        ];
+          nativeBuildInputs = buildDeps;
+          buildInputs = runtimeDeps;
+        };
+        dockerImage = pkgs.dockerTools.buildLayeredImage {
+          name = "mie";
+          tag = version;
+          config.Cmd = ["${self.packages.${system}.default}/bin/mie"];
+          contents =
+            [
+              self.packages.${system}.default
+            ]
+            ++ runtimeDeps;
+        };
+      };
+
+      devShells = {
+        default = pkgs.mkShell {
+          shellHook = ''
+            export  PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
+          '';
+          packages = devDeps ++ runtimeDeps ++ buildDeps;
+        };
       };
     });
-  };
 }
